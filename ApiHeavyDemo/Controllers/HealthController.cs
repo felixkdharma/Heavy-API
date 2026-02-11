68b4f6ddb88b76285d9bad69888eeabeaca29584
Felix Kurniawan Dharma <felixkd99@gmail.com>
2026-02-11 13:23:29 +0700

    Create API Health Check

---
+++ ApiHeavyDemo/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiHeavyDemo.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Check()
+        {
+            return Ok(new
+            {
+                status = "OK",
+                service = "ApiHeavyDemo",
+                time = DateTime.Now
+            });
+        }
+    }
+}
